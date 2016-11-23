cc1e367f4b413bc9e1633abcc6358b514bdda974
manojsoftware <manojitsoftware@gmail.com>
2016-11-22 22:18:29 -0600

    included trialmethod6() in CommentResource.java

--- src/main/java/org/manojit/messenger/resource/CommentResource.java
+++ src/main/java/org/manojit/messenger/resource/CommentResource.java
@@ -5,17 +5,22 @@ import javax.ws.rs.Path;
 @Path("/")
 public class CommentResource {
 
-public void trialmethod(){
-	
-}
-public void trialmethod3(){
-	
-}
-	public void trialmethod1(){
-		
-}
-	public void trialmethod4(){
-		
+	public void trialmethod() {
 
-}
+	}
+
+	public void trialmethod3() {
+
+	}
+
+	public void trialmethod1() {
+
+	}
+
+	public void trialmethod4() {
+
+	}
+	public void trailmethod6(){
+		System.out.println("this is trialmethod5()");
+	}
 }
